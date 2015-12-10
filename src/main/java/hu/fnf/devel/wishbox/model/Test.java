d09b8a124e9c56ff2a5d02a0248b534004455819
csikosbalint <csikos.balint@fnf.hu>
2015-12-10 15:31:30 +0100

    Init

---
+++ src/main/java/hu/fnf/devel/wishbox/model/Test.java
@@ -0,0 +1,7 @@
+package hu.fnf.devel.wishbox.model;
+
+public class Test {
+    public void test() {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/hu/fnf/devel/wishbox/model/TestTest.java
@@ -0,0 +1,10 @@
+package hu.fnf.devel.wishbox.model;
+
+public class TestTest {
+
+    @org.junit.Test
+    public void testTest1() throws Exception {
+        Test t = new Test();
+        t.test();
+    }
+}
